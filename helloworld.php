d6c77a54daa8f21be0ad3dfebc04cf743872c296
vagrant <vagrant@gaephp.local>
2014-05-11 02:04:46 +0000

    pipele

--- helloworld.php
+++ helloworld.php
@@ -9,7 +9,7 @@
     ?>
     <form action="/sign" method="post">
       <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
+      <div><input type="submit" value="Sign githubk"></div>
     </form>
   </body>
 </html>
