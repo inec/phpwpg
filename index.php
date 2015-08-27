b597da22cf66c800f38eb37af534401a36c1de1b
Y-Chi Lu <luyege+github@gmail.com>
2015-08-27 18:48:53 -0500

    templt

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 require 'vendor/autoload.php';
 $app = new Slim\Slim();
-$app->get('/', function () {
-    echo "<h1>Hello world</h1>";
+$app->get('/', function () {    echo "<h1>wPhpWPG Hello world</h1>";});
+
+$app->config(array(
+   'templates.path' => './templates'
+));
+$app->get('/about', function () use ($app) {
+    $app->render('about.php');
 });
 $app->run();
