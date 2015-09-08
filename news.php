48cbb0ae89186ef2584007f6eb389784bf3ad8ee
ine <inec@users.noreply.github.com>
2015-09-08 16:51:47 -0500

    Update news.php

--- news.php
+++ news.php
@@ -1,31 +1,69 @@
 <?php
   include_once 'bnav.php'; 
 
-  require_once 'config.php';
+/*   require_once 'config.php';
 require __DIR__ . '/vendor/autoload.php';
 
 const DEFAULT_PATH = '/NewsArticle';
-$firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+$firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN); */
 
 ?>
-<h1 style="margin-top:15px" align="center">Demo jQuery Bootstrap News Box </h1>
+	    <section id="aboutbrio">
+    <div class="container">
+        <div class="row">
+				<div class="text-center bgGFont opsix">NEWS</div>
+				  					  <!--		<div class="arrow_box">
+          <h1 class="logo">css arrow please!</h1>
+          </div>-->
+			<div class="text-center meetbrio">WHAT'S NEW AT BRIO</div>
+
+			
+        </div>
+
+    </div>
+
+        </section>
+	
+	    <!--HOME SECTION END  -->
+    <section id="aboutus" class="arrow_box">
+        <div class="hidden-xs container">
+            <div class="row text-center pad-bottom">
+
+
+							<h4 class="brlineheight">Fair warning: This section may seem a bit flighty. We're constantly looking for ways to get involved in our community, share interesting tips and info, or give our awesome employees the recognition they deserve, so there's always something new for us to share with you here! 
+							</h4>
+  
+
+            </div>
+
+        </div>
+
+
 
 <div class="container">
 <div class="row">
+<div class="col-md-4">
+<div class="panel panel-default">
+<div class="panel-heading"> <span class="glyphicon glyphicon-list-alt"></span><b> &nbsp;  NEWS AT BRIO</b></div>
+<div class="panel-body">
+<div class="row">
+<div class="col-xs-12">
+<ul id="nav">
+<?php 
+$string = file_get_contents("NewsArticle.json");
+$arrayj=json_decode($string,true);
 
-<div class="col-md-4">
-<div class="panel panel-default">
-<div class="panel-heading"> <span class="glyphicon glyphicon-list-alt"></span><b>News</b></div>
-<div class="panel-body">
-<div class="row">
-<div class="col-xs-12">
-<ul class="demo2">
-<li class="news-item">2nes dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-<li class="news-item">2-2ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-<li class="news-item">2-3psum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-<li class="news-item">2-4ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-<li class="news-item">2-5 ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
-<li class="news-item">2-6 ipsum dolor sit amet, consectetur adipiscing elit. Nullam in venenatis enim... <a href="#">Read more...</a></li>
+//echo sizeof($arrayj);
+$cnt=0;
+foreach ($arrayj as  $key =>&$value) {
+
+		echo "<li class='news-item' id="."><a href=#".$cnt."><strong>".$key."</strong></br>more...</a></li>";
+$cnt+=1;
+}//end of each
+
+
+?>
+
 </ul>
 </div>
 </div>
@@ -33,73 +71,78 @@ $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 <div class="panel-footer"> </div>
 </div>
 </div>
-<div class="col-md-4">
-<div class="panel panel-default">
-<div class="panel-heading"> <span class="glyphicon glyphicon-list-alt"></span><b>News</b></div>
-<div class="panel-body">
-<div class="row">
-<div class="col-xs-12">
-<ul id="demo3">
-<li class="news-item">3-1 porttitor ante eget hendrerit adipiscing. Maecenas at magna accumsan,
-										rhoncus neque id, fringilla dolor. <a href="#">Read more...</a></li>
-<li class="news-item">3-2 porttitor ante eget hendrerit adipiscing. Maecenas at magna accumsan,
-										rhoncus neque id, fringilla dolor. <a href="#">Read more...</a></li>
-<li class="news-item">3-3 ornare nisl lorem, ut condimentum lectus gravida ut. Ut velit nunc, vehicula
-										volutpat laoreet vel, consequat eu mauris. <a href="#">Read more...</a></li>
-<li class="news-item">3-4 ultrices tortor eu massa placerat posuere. Vivamus viverra sagittis nunc. Nunc
-										et imperdiet magna. Mauris sed eros nulla. <a href="#">Read more...</a></li>
-<li class="news-item">3-5 sodales tellus sit amet leo congue bibendum. Ut non mauris eu neque fermentum
-										pharetra. <a href="#">Read more...</a></li>
-<li class="news-item">3-6 suscipit orci sed viverra. Praesent at sollicitudin tortor, id sagittis
-										magna. Fusce massa sem, bibendum id. <a href="#">Read more...</a> </li>
-<li class="news-item">3-7 nec ligula sed est suscipit aliquet sed eget ipsum. Suspendisse id auctor
-										nibh. Ut porttitor volutpat augue, non sodales odio dignissi id. <a href="#">Read more...</a></li>
-<li class="news-item">3-8 bibendum consectetur diam, nec euismod urna venenatis eget. Cras consequat
-										convallis leo. <a href="#">Read more...</a> </li>
-</ul>
+<div class="col-md-8">
+<div id="tab-content">
+<?php 
+//$string = file_get_contents("NewsArticle.json");
+//$arrayj=json_decode($string,true);
+
+//echo sizeof($arrayj);
+foreach ($arrayj as  $key =>&$value) {
+
+
+if($key=="2015-09-08T04:48:12 02:00")
+{
+		echo "<div id=#".$key." >".array_values($value)[0]."</div>";
+}
+else
+{
+	echo "<div id=#".$key." >".array_values($value)[0]."</div>";
+}
+	}
+?>
 </div>
 </div>
-</div>
-<div class="panel-footer"> </div>
-</div>
+
 </div>
 </div>
-</div>
+    </section>
 <script type="text/javascript">
+$('#tab-content div').hide();
+$('#tab-content div:first').show();
+
+
     $(function () {
-        $(".demo1").bootstrapNews({
+
+
+        $("#nav").bootstrapNews({
             newsPerPage: 3,
-            autoplay: true,
-			pauseOnHover:true,
-            direction: 'up',
-            newsTickerInterval: 4000,
+            autoplay: false,
+                   navigation:false,
             onToDo: function () {
                 //console.log(this);
             }
         });
 		
-		$(".demo2").bootstrapNews({
-            newsPerPage: 4,
-            autoplay: true,
-			pauseOnHover: true,
-			navigation: false,
-            direction: 'down',
-            newsTickerInterval: 2500,
-            onToDo: function () {
-                //console.log(this);
-            }
-        });
+	$('#nav li').click(function(event) {
+    $('#nav li a').removeClass("active");
+    $(this).find('a').addClass("active");
+    $('#tab-content div').hide();
 
-        $("#demo3").bootstrapNews({
-            newsPerPage: 3,
-            autoplay: true,
-            
-            onToDo: function () {
-                //console.log(this);
-            }
-        });
+    var indexer = $(this).index(); //gets the current index of (this) which is #nav li
+	//var refid=$(this).find('a').attr('href');
+	//console.log($(this).html());
+	//console.log(  $(refid).html());
+    //$(refid).find('a').addClass("active");
+	//$(refid).fadeIn();
+	//event.preventDefault();
+	//$(this).find('a').attr('href').fadeIn();
+
+   $('#tab-content div:eq(' + indexer + ')').fadeIn(); //uses whatever index the link has to open the corresponding box 
+  // $(this).attr('id').fadeIn();
+}); //end of click
     });
 </script>
+<div class="container aboutbrio col-md-12">
+        <div class="row">
+				<div class="text-center bgGFont opsix"></div>
+
+			<div class="text-center meetbrio"></div>
+
+			
+        </div>
+
+ </div>
 <?php
    include_once 'bfooter.php'; 
 ?>	
