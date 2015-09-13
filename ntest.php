401230235f52d7c7d3c7d44f3d775e4a552711aa
Y-Chi Lu <luyege+github@gmail.com>
2015-09-13 13:34:55 -0500

    form

---
+++ ntest.php
@@ -0,0 +1,13 @@
+		<?php
+			// Place this in your between your <head> and </head> tags
+			require_once("nuggetz/init.php");
+		?>
+
+	4. Add a nugget to your page to create an instance of the nuggetz class
+
+		<?php new nuggetz('test');?>
+		<?php new nuggetzControl();?>
+
+	5. Add a login/logout facility to your page
+		
+	
