6ff9e45ec5421e28f9606ce604679d5b9cb84a13
Y-Chi Lu <luyege+github@gmail.com>
2015-09-19 20:52:08 -0500

    f

---
+++ oldfaq.php
@@ -0,0 +1,66 @@
+<?php
+  include_once 'bnav.php'; 
+ ?>
+ 
+  <!-- NAVBAR CODE END -->
+  
+	
+ <section id="driverlicense" class="aboutbrio">
+	<div class="container">
+        <div class="row">
+				<div class="text-center bgGFont opsix arrow-box">Insurance</div>
+			<div class="text-center meetbrio">FAQ</div>		
+        </div>
+    </div>
+</section >
+<div  class=" basic1">
+        <div class="container">
+        <section class=" row  pad-bottom">
+<h4>
+Does insurance lingo sound like Swedish? We get that. Here’s a friendly ‘phrasebook’ to help you feel right at home when you visit our offices! Still have questions? We’d be happy to help – just give us a <a href="#contact">call</a>!  
+</h4>		
+            <h3 id="education">Driver's Education</h3>
+			<h4>Did you know that in order to take Driver’s Ed in Manitoba, you have to become a client of Manitoba Public Insurance (MPI) first? That means that nope, you can’t sign up your child on their behalf – he or she has to be present, no exceptions.   </h4>
+			<h4>To become a client of MPI, we have to establish your identity in MPI’s database. To do that, we’ll need original documents that satisfy requirements proving your: 
+			<ul>
+			<li>	Birth date</li>
+			<li>	Legal name</li>
+			<li>	Signature</li>
+				<li>	Photograph</li>
+					<li>	Entitlement to be in Canada</li>
+
+
+			</ul>
+			
+			</h4>
+			
+			<h4>You’ll also need two (2) documents that prove that you live in Manitoba and your physical address. To help you figure out which documents you can bring, check out this <a href="#">list</a>. The most common combination of documents that new customers bring in are: 
+			<ul>
+			<li>		Valid Canadian passport, Manitoba Health card, and bank statement</li>
+			<li>		Canadian birth certificate, Manitoba Health card, and guarantor form</li>
+
+			</ul>
+			
+			</h4>
+			
+			<h4>If you’re under 18 years old, you’ll also need a parent or guardian with you. When you come in to sign up for Driver’s Ed, you’ll also have your photo taken. </h4>
+			<h4>Wondering where you can take the Driver’s Ed course? MPI has a handy course finder; use it <a href="#contact"> here</a>   </h4>
+
+			<h3 id="test">Licensing Tests</h3>
+			<h4>In order to get a driver’s license in Manitoba, you have to take a written test and a road test. Both can be taken through the school offering Driver’s Ed or an MPI Service Centre. </h4>
+			<h4>Next-day appointments for the written test are usually available at our branch! </h4>
+			<h4>If you have any questions, don’t hesitate to contact us  <a href="#contact"> here</a>. </h4>
+		</section>
+	</div>
+</div>
+
+	 <?php
+
+   include_once 'contactform.php'; 
+?>	
+    <!-- Footer Start-->
+
+ <?php
+
+   include_once 'bfooter.php'; 
+?>
