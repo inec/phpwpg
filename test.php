36cf9d2f5dcff88a2ee114298af52e87ed52fca1
Y-Chi Lu <luyege+github@gmail.com>
2015-09-23 00:17:30 -0500

    t

---
+++ test.php
@@ -0,0 +1,149 @@
+	<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset='UTF-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="YChi Lu" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Brio Insurance</title>
+    <!-- BOOTSTRAP STYLE SHEET -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICONS STYLE SHEET -->
+    <!-- <link href="assets/css/font-awesome.css" rel="stylesheet" />-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- CUSTOM STYLES -->
+	
+    <link href="assets/css/style.css" rel="stylesheet" />
+	<link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="scripts/jquery.bootstrap.newsbox.min.js" type="text/javascript"></script>
+
+	<!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+<img src="assets/img/vivaldi-logo.png" alt="" class=" img-responsive" />
+                
+				
+                </a>
+				
+            </div>
+
+            <div class="navbar-collapse collapse  scroll-me">
+
+					<ul class="nav navbar-nav">
+				     <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+<li class="dropdown">
+              <a href="about.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ABOUT <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="about.php#aboutbrio">Meet Brio</a></li>
+                <li><a href="about.php#career">Careers</a></li>
+                <li><a href="about.php#community">Giving Back</a></li>
+ <li><a href="about.php#contact">Contact</a></li>
+              </ul>
+            </li>
+
+				
+
+					
+                    <li><a href="product.php">PRODUCTS</a></li>
+                    <li><a href="news2.php">NEWS</a></li>
+                    <li><a href="#quote">REQUEST A QUOTE</a></li>
+					    <li><a href="faq.php">FAQ</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    <!-- NAVBAR CODE END -->
+	<?php 
+
+
+  include_once 'sidebar.php';
+  
+
+?>
+    <!-- Footer Start-->
+
+
+<footer class="row">
+		
+		<div class="col-md-3 col-md-offset-1 text-left visible-xs">
+		<img src="assets/img/bird.gif" alt="" class="col-sm-offset-0" height="30">				<i class="fa fa-copyright"></i>  <?php echo date("Y")?>  Brio Insurance
+		</div> <!-- End of duplicate logo-->
+
+	<div class="col-md-5 col-md-offset-1">
+	
+	<div class="row col-md-12 hidden-xs">&nbsp;</div>
+	<div class="footerline hidden-xs">&nbsp;</div>
+<div class="footerline hidden-xs">&nbsp;</div>
+
+	<div class="row brhr ">
+	</div>
+	
+
+<div>
+
+				<div class="col-md-6 col-xs-6 text-left">Stay Connected&nbsp;</div>
+				
+				<div class="col-md-6 col-xs-6 text-right">			
+			
+					<a href="https://www.facebook.com/brioinsurance"><i class=" fa fa-facebook fa-1x "></i></a>
+					<a href="https://twitter.com/SunovaCU">	<i class=" fa fa-twitter fa-1x "></i></a>
+					
+					<a href="#">	<i class="fa fa-envelope-o"></i></a>
+					
+					<a href="#">	<span class="glyphicon glyphicon-map-marker" ></span></a>
+					
+					<a href="#">	<i class="fa fa-tablet"></i></a>
+						<a href="https://instagram.com/sunovacu/" class="hidden-xs"><i class="fa fa-instagram"></i></a>
+						<a href="#"><i class="fa fa-linkedin-square"></i></a>
+				</div>
+				<div class="row brhr visible-xs"></div>
+</div>
+				
+
+				</div>
+       <!--start duplicated logo-->
+			<div class="col-md-3 col-md-offset-1 text-right hidden-xs">
+				<div class="footerline hidden-xs">&nbsp;</div>
+		<div class="row brhr">	<img src="assets/img/bird.gif" alt="" class=" col-xs-offset-4 col-sm-offset-0 pull-right" height="60"></div>
+
+	
+	
+			<!--<span class="glyphicon glyphicon-copyright-mark" ></span>-->
+			<i class="fa fa-copyright"></i>  <?php echo date("Y")?> <a href="http://www.BrioInsurance.ca/" target="_blank"> Brio Insurance</a>
+			</div>
+    </footer>
+    <!-- FOOTER SECTION END-->
+    <!-- REQUIRED SCRIPTS FILES -->
+
+    <!-- REQUIRED BOOTSTRAP SCRIPTS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	
+    <!-- BACKGROUND VIDEO PLUGIN  -->
+
+    <!-- SCROLLING SCRIPTS PLUGIN  -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <!-- CUSTOM SCRIPTS   -->
+	 <script src="assets/js/sidebar.js" type="text/javascript"></script>
+	
+	
+	</body>
+</html>
