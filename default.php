daf8522cf9024f72ec8530150c571fe73c8cdedb
Y-Chi Lu <luyege+github@gmail.com>
2015-10-11 19:53:43 -0500

    route

---
+++ default.php
@@ -0,0 +1,50 @@
+<?php
+  include_once 'bnav.php'; 
+ ?>
+ 
+  <!-- NAVBAR CODE END --> 
+  
+    <div id="home" class="shadow-bot-darkgreen">
+			<!--<img src="assets/img/Big_Bird.png" alt="" class="hidden-sm hidden-xs pull-right bigbird" >-->
+        <div class="overlay">
+         <!-- overylay class usage -->
+	
+            <div class="container">
+					
+				<div class="footerline hidden-xs">&nbsp;</div>
+			<div class="row">
+
+   <div class="col-md-7 col-xs-12">
+			  	<h1 class=" color1 fweight3 hidden-xs fontopens pad-top50">Welcome to Brio Insurance! </h1>
+					  	<h2 class=" color1 visible-xs fontopens ">Welcome to Brio Insurance </h2>
+                <div class="text-left">
+
+                
+                  
+                    <p class="p-cls fontopens">
+                         Our approachable and helpful staff has been providing peace of mind and protection in Manitoba for nearly 20 years. Our goal is to always keep your best interests at heart, to make sure you get the right product and service for you! 
+						 <br/>
+						 <br/>
+						<span class="colornormal fontrobotos"> Learn more about Brio Insurance </span><a href='about.php' class="colornormal fontrobotos">here</a>.
+                    </p>
+					
+
+                </div>	</div>
+						
+		<!--<img src="assets/img/Big_Bird.png" alt="" class="hidden-sm hidden-xs pull-right bigbird" >-->
+            </div>
+
+			</div>
+			
+        </div>
+
+    </div>
+
+		
+		
+
+
+ <?php
+
+   include_once 'bfooter.php'; 
+?>
