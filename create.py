74c5520906b83726434cfb703c33529d92a593d1
Y-Chi Lu <luyege+github@gmail.com>
2016-02-15 11:11:17 -0600

    mongo1 py1

--- create.py
+++ create.py
@@ -5,6 +5,12 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.test
-cursor = db.restaurants.find({"cuisine": "Italian", "address.zipcode": "10075"})
-for document in cursor:
-    print(document)
+
+posts=db.posts
+post={'author':'rodrigo','text':'Hello World'}
+id_p=posts.insert(post)
+print 'crete the id: %s'%post
+client.close()
+# cursor = db.restaurants.find({"cuisine": "Italian", "address.zipcode": "10075"})
+# for document in cursor:
+#   print(document)
