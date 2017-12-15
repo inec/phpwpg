9ad42fc368cdfb20e78d2c341595f40bbc445aab
Y-Chi Lu <ylu@sunovacu.ca>
2017-12-15 15:26:54 -0600

    'tp.php'

---
+++ tp.php
@@ -0,0 +1,18 @@
+<?php
+ // phpinfo(); // all info
+  // module info, phpinfo(8) identical
+ // phpinfo(INFO_MODULES);
+
+require __DIR__ . '/vendor/autoload.php';
+//phpinfo();
+$templates = new League\Plates\Engine('./templates');
+
+//echo $templates->render('partials/header');
+//echo $templates->render('profile', ['name' => 'Jonathan']);
+
+
+// Render a template in a subdirectory
+
+// Render a template
+echo $templates->render('profile', ['name' => 'Wpg']);
+?>
