4373e126f9af89d66ab5b948b0473a4a01827fc7
Y-Chi Lu <ylu@sunovacu.ca>
2017-12-15 15:57:54 -0600

    test

---
+++ template.php
@@ -0,0 +1,14 @@
+<?php require __DIR__ . '/vendor/autoload.php'; ?>
+
+<html>
+<head>
+    <title><?=$title?></title>
+</head>
+<body>
+
+<main>
+  atemplate  <?=$this->section('content')?>
+</main>
+
+</body>
+</html>
