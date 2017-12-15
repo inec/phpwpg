76e35c7aa5687f6cae4f8f2f7c5fd0878b73b12a
Y-Chi Lu <ylu@sunovacu.ca>
2017-12-15 15:31:10 -0600

    try

---
+++ user_profile.php
@@ -0,0 +1,8 @@
+<?php // user_profile.php ?>
+
+<?php $this->insert('header', ['title' => 'User Profile']) ?>
+
+<h1>User Profile</h1>
+<p>Hello, <?=$this->escape($name)?></p>
+
+<?php $this->insert('footer') ?>
