7121152ff365af8475d64e3b0dcd683089ba3139
ine <inec@users.noreply.github.com>
2017-12-15 15:24:53 -0600

    Update indexTP.php

--- indexTP.php
+++ indexTP.php
@@ -8,11 +8,11 @@ require __DIR__ . '/vendor/autoload.php';
 $templates = new League\Plates\Engine('./templates');
 
 //echo $templates->render('partials/header');
-echo $templates->render('profile', ['name' => 'Jonathan']);
-/*
+//echo $templates->render('profile', ['name' => 'Jonathan']);
+
 
 // Render a template in a subdirectory
 
 // Render a template
-echo $templates->render('profile', ['name' => 'Jonathan']);*/
+echo $templates->render('profile', ['name' => 'Wpg']);
 ?>
