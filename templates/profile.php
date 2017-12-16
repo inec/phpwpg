4a8bc50ad1eacf29a0ed14e5718c974f7eab624d
ine <inec@users.noreply.github.com>
2017-12-15 23:17:32 -0600

    't'

--- templates/profile.php
+++ templates/profile.php
@@ -3,3 +3,9 @@
 <h1>1 profile.User Profile</h1>
 
 <p>2. Hello, <?=$this->e($name)?></p>
+
+<!-- Example without using batch -->
+<p>3. Welcome = <?=$this->escape(strtoupper(strip_tags($name)))?></p>
+
+
+<p>4. Welcome   </p>
--- user_profile.php
+++ user_profile.php
@@ -7,3 +7,5 @@
 
 <h1>User Profile</h1>
 <p>Hello, <?=$this->escape($name)?></p>
+
+
