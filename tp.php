aca4542015507c3c992a690c51e4edcb05c5d90d
ine <inec@users.noreply.github.com>
2017-12-15 23:10:16 -0600

    'twha'

--- tp.php
+++ tp.php
@@ -5,12 +5,10 @@ require __DIR__ . '/vendor/autoload.php';
 
 $templates = new League\Plates\Engine('./templates');
 
-$template 
-= new League\Plates\Template\Template($templates, 'profile');
+$template = new League\Plates\Template\Template($templates, 'profile');
 
 // Render the template
 echo $template->render(['name' => 'TTTg']);
-
 // You can also render the template using the toString() magic method
 
 
