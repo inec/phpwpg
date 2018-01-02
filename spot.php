b37eff876d50b038d8d5cbc1a1f3e08339e05670
ine <inec@users.noreply.github.com>
2018-01-01 19:12:46 -0600

    'autoload-required'

--- spot.php
+++ spot.php
@@ -1,6 +1,10 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
 $cfg = new \Spot\Config();
-// MySQL
-$cfg->addConnection('mysql', 'mysql://user:password@localhost/database_name');
+// MySQL$cfg->addConnection('mysql', 'mysql://user:password@localhost/database_name');
 // Sqlite
-$cfg->addConnection('sqlite', 'sqlite://path/to/database.sqlite');
+$cfg->addConnection('sqlite', 'sqlite:./database.sqlite');
 $spot = new \Spot\Locator($cfg);
+echo 'test';
+?>
